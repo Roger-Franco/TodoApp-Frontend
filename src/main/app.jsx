bfe09a9a9e47689fffdca398d325e2e22e0628de
Roger-Franco <rogerfrancogalt@gmail.com>
2022-01-21 16:14:19 -0400

    Componente App

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,7 +1,8 @@
-import 'modules/bootstrap/dist/css/bootstrap.min.css' import 'modules/font-awesome/css/font-awesome.min.css'
-import React from 'react'
+import "modules/bootstrap/dist/css/bootstrap.min.css";
+import "modules/font-awesome/css/font-awesome.min.css";
+import React from "react";
 export default props => (
-  <div className='container'>
+  <div className="container">
     <h1>Teste</h1>
   </div>
-)
+);
